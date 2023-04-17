f82a349cbf3952f893b16f649c08b98fc10a76a9
AdamLeijman <adam.leijman@hotmail.com>
2023-04-17 12:29:10 +0200

    Lagt till en highscorelista och gjort så den sorteras och kollar efter varje runda om man platsar på listan

--- controller/Controller.java
+++ controller/Controller.java
@@ -1,8 +1,11 @@
 package controller;
 
+import model.HighScore;
 import view.QuizView;
 import model.Question;
 import model.Quiz;
+
+import javax.swing.*;
 
 /**
  * controller class that control our project.
@@ -15,15 +18,18 @@ public class Controller {
     private QuizView quizView;
     private int score;
     private int lastScore;
+    private HighScore list;
 
 
     /**
      * Parameter constrictor that used to create object of Controller.
+     *
      * @param fileName file name that contains questions.
      */
     public Controller(String fileName) {
         quiz = new Quiz(fileName);
         this.quizView = new QuizView(this);
+        list = new HighScore();
         this.quizView.updateQuestion(quiz.getCurrentQuestion(), quiz.getCurrentQuestionNum(), quiz.getTotalNumberQuestions());
         this.score = 0;
         this.quizView.display();
@@ -37,13 +43,13 @@ public class Controller {
             this.quiz.prevQuestion();
             this.quizView.updateQuestion(quiz.getCurrentQuestion(), quiz.getCurrentQuestionNum(), quiz.getTotalNumberQuestions());
         }
-        if(this.quiz.getCurrentQuestionNum() == 1){
+        if (this.quiz.getCurrentQuestionNum() == 1) {
             this.quizView.disablePrevButton();
         }
     }
 
     public void nextQuestion() {
-        if(this.quizView.getUserAnswer().equals("")){
+        if (this.quizView.getUserAnswer().equals("")) {
             this.quizView.showError("Must select one option first");
             return;
         }
@@ -53,15 +59,15 @@ public class Controller {
         if (this.quiz.getCurrentQuestion().isCorrect(this.quizView.getUserAnswer())) {
             lastScore = 1;////presentag
             this.quizView.showRightOrWrong("You answered correct!");
-        }
-        else{
+        } else {
             lastScore = 0;
             this.quizView.showRightOrWrong("You answered wrong!");
         }
         this.score += lastScore;
         if (!(this.quiz.hasNextQuestion())) {
 //presentag
-            this.quizView.showGameOverMessage("game over", this.score * 100/ this.quiz.getTotalNumberQuestions());
+            this.quizView.showGameOverMessage("game over", this.score * 100 / this.quiz.getTotalNumberQuestions());
+            onLeaderboard();
             return;
         }
         this.quizView.clearSelection();
@@ -69,5 +75,43 @@ public class Controller {
         this.quizView.enablePrevButton();
         this.quizView.updateQuestion(quiz.getCurrentQuestion(), quiz.getCurrentQuestionNum(), quiz.getTotalNumberQuestions());
 
+    }
+
+    /**
+     * Checks if the score is better than the last person on the leaderboard
+     * and if it is it asks for your name and puts it in the leaderboard and the sorts it.
+     */
+    public void onLeaderboard() {
+        getList();
+        int[] listScore = list.getScore();
+        String[] listName = list.getName();
+
+        if (score > listScore[9]) {
+            String name = JOptionPane.showInputDialog(null, "You are in the Top10! \n" + "Enter name");
+            listScore[9] = score;
+            listName[9] = name;
+        }
+        for (int i = 0; i < listScore.length; i++) {
+            for (int j = i + 1; j < listScore.length; j++) {
+                int tmp;
+                String temp;
+                if (listScore[i] < listScore[j]) {
+                    tmp = listScore[i];
+                    listScore[i] = listScore[j];
+                    listScore[j] = tmp;
+                    temp = listName[i];
+                    listName[i] = listName[j];
+                    listName[j] = temp;
+                }
+            }
+        }
+        list.writeToList(listName, listScore);
+    }
+
+    /**
+     * @return the highscorelist to the mainframe.
+     */
+    public String[] getList() {
+        return list.readList();
     }
 }
---
+++ model/HighScore.java
@@ -0,0 +1,75 @@
+package model;
+
+import java.io.*;
+import java.util.Scanner;
+
+
+/**
+ * Class that reads the text file HighScore and changes it if there is a new one.
+ *
+ * @author Adam Leijman, Mattias Malm
+ */
+public class HighScore {
+
+    private String[] name = new String[10];
+    private int[] score = new int[10];
+    private String[] list = new String[10];
+    private int place = 1;
+
+    /**
+     * Inputs a new person to the highScoreList.
+     * @param name  the name of the person.
+     * @param moves how many moves it took to win.
+     */
+    public void writeToList(String[] name, int[] moves) {
+        try {
+            FileWriter myWriter = new FileWriter("files/HighScoreList.txt");
+            for(int i = 0; i < name.length; i++) {
+                myWriter.write(name[i] + "\n" + moves[i] + "\n");
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("Could not write to HighScoreList");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Prints the text from HighScoreList.
+     * @return the highscorelist
+     */
+    public String[] readList() {
+        int i = 0;
+        try {
+            File myFile = new File("files/HighScoreList.txt");
+            Scanner myReader = new Scanner(myFile);
+            while (myReader.hasNextLine()) {
+                name[i] = myReader.nextLine();
+                score[i] = Integer.parseInt(myReader.nextLine());
+                if (name[i] != null) {
+                    list[i] = String.format(place + " " + name[i] + " " + score[i]);
+                    i++;
+                    place++;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred");
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    /**
+     * @return The scores from the highscorelist
+     */
+    public int[] getScore() {
+        return score;
+    }
+
+    /**
+     * @return The names from the highscorelist
+     */
+    public String[] getName() {
+        return name;
+    }
+}

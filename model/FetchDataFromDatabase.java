0db379f4b599e27489e53484bc2773c78ca00ed2
martin24olsson <128680764+martin24olsson@users.noreply.github.com>
2023-04-18 10:27:26 +0200

    top 100 spelare

--- model/FetchDataFromDatabase.java
+++ model/FetchDataFromDatabase.java
@@ -2,6 +2,8 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -14,7 +16,7 @@ public class FetchDataFromDatabase {
 
     public Connection getDatabaseConnection() {
 
-        String url = "jdbc:postgresql://pgserver.mau.se/onlinestore";
+        String url = "jdbc:postgresql://pgserver.mau.se/gissa_fotbollsspelare";
         Properties props = new Properties();
         props.setProperty("user", "an5411");
         props.setProperty("password", "7sbqoxrh");
@@ -23,6 +25,26 @@ public class FetchDataFromDatabase {
         try {
             Connection conn = DriverManager.getConnection(url, props);
             System.out.println("Connection Established");
+
+
+            String QUERY = "SELECT * FROM \"fotbollsspelare\"";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(QUERY);
+            int count =0;
+            while (rs.next()) {
+                if (count>100){
+                    break;
+                }
+                System.out.print("ID: " + rs.getString("id"));
+                System.out.print(", Name: " + rs.getString("name"));
+                System.out.println("----------------------------------------------------------");
+                count++;
+            }
+
+            stmt.close();
+            conn.close();
+
+
             return conn;
 
         } catch (Exception e) {

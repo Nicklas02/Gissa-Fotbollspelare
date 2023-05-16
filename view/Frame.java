ebb93dc4c61ad06d7e6c1c926dcdb0a1502d5fa8
Mohamed Alsaghir <117099731+Aa0994059271AAa@users.noreply.github.com>
2023-05-16 19:35:44 +0200

    Ändring i Frame
    Jag har tagit bort gamal Ligas funktioner och gjorde en ComboBox för Alla GameTaype och difficultyOptions

--- view/Frame.java
+++ view/Frame.java
@@ -20,11 +20,17 @@ public class Frame extends JFrame {
     private int height = 800;
     private Font font = new Font("Arial", Font.BOLD, 24);
     private JTextArea scoresJTextArea;
-    private JRadioButton[] difficultyOptions;
 
+    private JComboBox <String> difficultyOptions;
+    private JComboBox<String> gameTypeOptions;
+
+
+    /*
+     private JRadioButton[] difficultyOptions;
     private ButtonGroup difficultyOptionsGroup;
     private JRadioButton[] gameTypeOptions;
     private ButtonGroup gameTypeOptionsGroup;
+     */
     private Controller2 controller;
 
     public Frame(Controller2 controller) {
@@ -51,12 +57,6 @@ public class Frame extends JFrame {
         startPanel.add(label);
         startPanel.add(scrollPane);
         startPanel.add(showlist);
-        for (JRadioButton option : difficultyOptions) {
-            startPanel.add(option);
-        }
-        for (JRadioButton option : gameTypeOptions) {
-            startPanel.add(option);
-        }
         startPanel.add(backgroundLabel);
         startPanel.setBounds(0, 0, width, height);
         this.add(startPanel);
@@ -150,89 +150,52 @@ public class Frame extends JFrame {
     }
 
     private void createRadioButtons() {
-        difficultyOptions = new JRadioButton[2];
-        difficultyOptionsGroup = new ButtonGroup();
-        difficultyOptions[0] = new JRadioButton("Normal");
-        difficultyOptions[0].setBounds(640, height / 2 + 1, 120, 30);
-        difficultyOptions[0].setFont(font);
-        difficultyOptions[0].setSelected(true);
-        difficultyOptions[0].setBackground(Color.LIGHT_GRAY);
-        difficultyOptions[0].setForeground(Color.WHITE);
-        difficultyOptionsGroup.add(difficultyOptions[0]);
+        String[] diffOptions = {"Normal", "Hard"};
+        difficultyOptions = new JComboBox<>(diffOptions);
+        difficultyOptions.setBounds(640, height / 2 + 1, 120, 30);
+        difficultyOptions.setSelectedIndex(0);
+        difficultyOptions.setBackground(Color.LIGHT_GRAY);
+        difficultyOptions.setForeground(Color.WHITE);
 
-        difficultyOptions[1] = new JRadioButton("Hard");
-        difficultyOptions[1].setBounds(640, height / 2 + 50, 100, 30);
-        difficultyOptions[1].setFont(font);
-        difficultyOptions[1].setBackground(Color.LIGHT_GRAY);
-        difficultyOptions[1].setForeground(Color.WHITE);
-        difficultyOptionsGroup.add(difficultyOptions[1]);
-        /////////////////////////////////////////////////
-        gameTypeOptions = new JRadioButton[5];
-        gameTypeOptionsGroup = new ButtonGroup();
-        gameTypeOptions[0] = new JRadioButton("PremierLeague");
-        gameTypeOptions[0].setBounds(420, height / 2 + 1, 200, 30);
-        gameTypeOptions[0].setFont(font);
-        gameTypeOptions[0].setSelected(true);
-        gameTypeOptions[0].setBackground(Color.LIGHT_GRAY);
-        gameTypeOptions[0].setForeground(Color.WHITE);
-        gameTypeOptionsGroup.add(gameTypeOptions[0]);
-
-        gameTypeOptions[1] = new JRadioButton("LaLiga");
-        gameTypeOptions[1].setBounds(420, height / 2 + 170, 120, 30);
-        gameTypeOptions[1].setFont(font);
-        gameTypeOptions[1].setBackground(Color.LIGHT_GRAY);
-        gameTypeOptions[1].setForeground(Color.WHITE);
-        gameTypeOptionsGroup.add(gameTypeOptions[1]);
-
-        gameTypeOptions[2] = new JRadioButton("Bundesliga");
-        gameTypeOptions[2].setBounds(420, height / 2 + 50, 170, 30);
-        gameTypeOptions[2].setFont(font);
-        gameTypeOptions[2].setBackground(Color.LIGHT_GRAY);
-        gameTypeOptions[2].setForeground(Color.WHITE);
-        gameTypeOptionsGroup.add(gameTypeOptions[2]);
-
-        gameTypeOptions[3] = new JRadioButton("Ligue1");
-        gameTypeOptions[3].setBounds(420, height / 2 + 90, 120, 30);
-        gameTypeOptions[3].setFont(font);
-        gameTypeOptions[3].setBackground(Color.LIGHT_GRAY);
-        gameTypeOptions[3].setForeground(Color.WHITE);
-        gameTypeOptionsGroup.add(gameTypeOptions[3]);
-
-        gameTypeOptions[4] = new JRadioButton("SerieA");
-        gameTypeOptions[4].setBounds(420, height / 2 + 130, 120, 30);
-        gameTypeOptions[4].setFont(font);
-        gameTypeOptions[4].setBackground(Color.LIGHT_GRAY);
-        gameTypeOptions[4].setForeground(Color.WHITE);
-        gameTypeOptionsGroup.add(gameTypeOptions[4]);
+        //***************************//
+        String[] gameOption = {"PremierLeague", "LaLiga", "Bundesliga", "Ligue1", "SerieA"};
+        gameTypeOptions.setBounds(420, height / 2 + 1, 200, 30);
+        gameTypeOptions.setFont(font);
+        gameTypeOptions.setSelectedIndex(0);
+        gameTypeOptions.setBackground(Color.LIGHT_GRAY);
+        gameTypeOptions.setForeground(Color.WHITE);
     }
 
     public GameType getGameType() {
-        if (gameTypeOptions[0].isSelected()) {
-            return GameType.PremierLeague;
-        } else if (gameTypeOptions[1].isSelected()) {
-            return GameType.LaLiga;
-        } else if (gameTypeOptions[2].isSelected()) {
-            return GameType.Bundesliga;
-        } else if (gameTypeOptions[3].isSelected()) {
-            return GameType.Ligue1;
-        } else if (gameTypeOptions[4].isSelected()) {
-            return GameType.SerieA;
+        String selectedGameType = (String) gameTypeOptions.getSelectedItem();
+        switch (selectedGameType){
+            case "PremierLeague":
+                return GameType.PremierLeague;
+            case "LaLiga":
+                return GameType.LaLiga;
+            case "Bundesliga":
+                return GameType.Bundesliga;
+            case "Ligue1":
+                return GameType.Ligue1;
+            case "SerieA":
+                return GameType.SerieA;
+            default:
+                return null;
         }
-        return null;
-    }
+        }
 
 
-
-    public Difficulty getDifficulty(){
-        if(difficultyOptions[0].isSelected()){
+        
+    public Difficulty getDifficulty() {
+        String selectedDifficulty = (String) difficultyOptions.getSelectedItem();
+        if (selectedDifficulty.equals(NORMAL)) {
             return Difficulty.Normal;
+        } else {
+            return Difficulty.Hard;
         }
-        return Difficulty.Hard;
     }
-
-    public String getPlayerName(){
-        return playerNameJTextField.getText().trim();
-    }
-
+        public String getPlayerName () {
+            return playerNameJTextField.getText().trim();
+        }
 
 }
